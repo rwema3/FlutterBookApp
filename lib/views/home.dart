e79dd62fa6da4ccb66b0999708e5e7cd719339b6
rwema3 <rwemadominik3@gmail.com>
2022-01-21 16:31:56 +0200

    Flutter book app

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -220,7 +220,7 @@ class BooksTile extends StatelessWidget {
                             Row(
                               children: <Widget>[
                                 StarRating(
-                                  rating: ratin,
+                                  rating: rating,
                                 ),
                                 Spacer(),
                                 Text(categorie,style: TextStyle(
