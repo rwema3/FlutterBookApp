7dd5b3564330a762c040aea3c4824426122c1f2e
rwema3 <rwemadominik3@gmail.com>
2022-01-21 16:30:33 +0200

    Flutter book app

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -208,7 +208,7 @@ class BooksTile extends StatelessWidget {
                           children: <Widget>[
                             Text(title, style: TextStyle(
                                 color: Colors.black87,
-                                fontSize: ,
+                                fontSize: 1,
                                 fontWeight: FontWeight.w500
                             ),),
                             SizedBox(height: 8,),
