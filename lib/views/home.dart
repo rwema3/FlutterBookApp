adb6204ef86154b056f903d8fafc7b682254b5d4
rwema3 <rwemadominik3@gmail.com>
2022-01-21 16:28:17 +0200

    Flutter book app

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -123,7 +123,7 @@ class CategorieTile extends StatefulWidget {
 
   final String text;
   final bool isSelected;
-  CategorieTile({this.text, @required this.isSelec});
+  CategorieTile({this.text, @required this.isSelect});
 
   @override
   _CategorieTileState createState() => _CategorieTileState();
