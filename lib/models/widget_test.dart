3fc1b417d2608136cd767ee47560e58ba16d86f9
rwema3 <rwemadominik3@gmail.com>
2022-01-30 19:37:33 +0200

    Flutter book app

--- lib/models/widget_test.dart
+++ lib/models/widget_test.dart
@@ -17,9 +17,9 @@ void main() {
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
 
-    // Tap the '+'
+
+    //
 
 
 
