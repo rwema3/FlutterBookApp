fc10be5e582059814526deb7ce705cbf2e8ac2c6
rwema3 <rwemadominik3@gmail.com>
2022-01-30 19:46:11 +0200

    Flutter book app

--- lib/models/widget_test.dart
+++ lib/models/widget_test.dart
@@ -11,8 +11,8 @@ import 'package:flutter_test/flutter_test.dart';
 
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build
+  testWidgets('Counter increments smoke test', (WidgetTester tester)  {
+    //
 
     //
 
