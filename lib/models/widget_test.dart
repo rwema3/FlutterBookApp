b1a6ca98bddad27c262fbc1ac0bb12c6b001c74c
rwema3 <rwemadominik3@gmail.com>
2022-01-30 19:43:27 +0200

    Flutter book app

--- lib/models/widget_test.dart
+++ lib/models/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:booksh';
+import 'package:books';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
