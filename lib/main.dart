8ffd0728b609899b7f305f141fa2eb994d53a80d
rwema3 <rwemadominik3@gmail.com>
2022-02-06 20:38:26 +0200

    // This widget

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget 
+  // This widget
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
