fa3407aa5c7496b400fc51c5769f7187a1922163
rwema3 <rwemadominik3@gmail.com>
2022-02-06 20:35:56 +0200

    News App

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root
+  // This widget is the 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
