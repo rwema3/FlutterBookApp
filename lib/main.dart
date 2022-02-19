ee7d97f64b2382d1e78d9a9dc09d70549d663050
Rwema <52289151+rwema3@users.noreply.github.com>
2022-02-19 23:00:12 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This wiget is the rou
+  // This wiget is the ro
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
