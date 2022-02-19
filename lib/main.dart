1076f1d5d723f12a6484dd532d07c492e9e6ea39
Rwema <52289151+rwema3@users.noreply.github.com>
2022-02-19 22:58:45 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of our 
+  // This widget is the root o our 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
